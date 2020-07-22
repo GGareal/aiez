e6312c54e29f207327eb33a7e783cba5973863ac
JeongminPark <wjd326als@naver.com>
2020-07-23 01:08:44 +0900

    Updates

--- src/components/About.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class About extends React.Component {
-  render() {
-    return <div>Hello this is about</div>;
-  }
-}
-
-export default About;
--- src/components/More_models.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class More_models extends React.Component {
-  render() {
-    return <div>Hello this is about</div>;
-  }
-}
-
-export default More_models;
